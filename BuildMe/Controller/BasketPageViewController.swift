c905955505724ae5257539fe3b28c5592609007f
nazrinsultanli <nazrindolkhanova@gmail.com>
2023-11-05 12:11:55 +0400

    firstTry

---
+++ BuildMe/Controller/BasketPageViewController.swift
@@ -0,0 +1,20 @@
+//
+//  BasketPageViewController.swift
+//  BuildMe
+//
+//  Created by Nazrin Sultanlı on 04.11.23.
+//
+
+import UIKit
+
+class BasketPageViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+
+}
---
+++ BuildMe/Controller/HomePageViewController.swift
@@ -0,0 +1,24 @@
+//
+//  HomePageViewController.swift
+//  BuildMe
+//
+//  Created by Nazrin Sultanlı on 04.11.23.
+//
+
+import UIKit
+
+class HomePageViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = "BuildMe"
+        //button.imageEdgeInsets = UIEdgeInsetsMake(25,25,25,25)
+
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+
+
+}
