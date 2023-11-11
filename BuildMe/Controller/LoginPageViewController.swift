3bea4f3b37f2ba54a1030ec1333e8d36f7a6456f
nazrinsultanli <nazrindolkhanova@gmail.com>
2023-11-11 12:46:35 +0400

    LoginPage

--- BuildMe/Controller/LoginPageViewController.swift
+++ BuildMe/Controller/LoginPageViewController.swift
@@ -9,42 +9,43 @@ import UIKit
 
 class LoginPageViewController: UIViewController {
 
-//    
-//    @IBOutlet weak var buttonView: UIButton!
-//    @IBOutlet weak var emailTextField: UITextField!
-//    
-//    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var buttonView: UIButton!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var emailTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
 //        UserDefaults.standard.setValue(true, forKey: "LoggedIn")
         
         
-        //setupUI()
+        setupUI()
         
     }
     
     private func setupUI(){
-  //          self.view.backgroundColor = UIColor(patternImage: UIImage(named: "launchBackground")!)
+            self.view.backgroundColor = UIColor(patternImage: UIImage(named: "launchBackground")!)
             
-//            emailTextField.layer.cornerRadius = 25
-//            emailTextField.layer.borderWidth = 1
-//            emailTextField.layer.borderColor = UIColor.white.cgColor
-//            
-//            passwordTextField.layer.cornerRadius = 25
-//            passwordTextField.layer.borderWidth = 1
-//            passwordTextField.layer.borderColor = UIColor.white.cgColor
-//            
-//            buttonView.layer.cornerRadius = 25
-//            buttonView.layer.borderWidth = 1
-//            //buttonView.layer.borderColor = UIColor.black.cgColor
-//            
-//            emailTextField.layer.masksToBounds = true
-//            passwordTextField.layer.masksToBounds = true
-//            buttonView.layer.masksToBounds = true
+            emailTextField.layer.cornerRadius = 25
+            emailTextField.layer.borderWidth = 1
+            emailTextField.layer.borderColor = UIColor.white.cgColor
+            
+            passwordTextField.layer.cornerRadius = 25
+            passwordTextField.layer.borderWidth = 1
+            passwordTextField.layer.borderColor = UIColor.white.cgColor
+            
+            buttonView.layer.cornerRadius = 25
+            buttonView.layer.borderWidth = 1
+            //buttonView.layer.borderColor = UIColor.black.cgColor
+            
+            emailTextField.layer.masksToBounds = true
+            passwordTextField.layer.masksToBounds = true
+            buttonView.layer.masksToBounds = true
             
         }
     
+    @IBAction func logInClicked(_ sender: Any) {
+    }
     
     
 //    
