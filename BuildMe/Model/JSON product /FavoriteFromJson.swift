98fd93e3543a8b1c9493dbf15eb1f43ce1b0c997
nazrinsultanli <nazrindolkhanova@gmail.com>
2023-11-21 00:25:43 +0400

    jsonFav

---
+++ BuildMe/Model/JSON product /FavoriteFromJson.swift
@@ -0,0 +1,29 @@
+//
+//  UsersFromJson.swift
+//  MyBankApp
+//
+//  Created by Nazrin Sultanlı on 17.10.23.
+//
+
+import Foundation
+
+class FavoriteFromJson {
+    
+    static let shared = FavoriteFromJson()
+    var favProducts = [PRODUCTJs]()
+    let helper = FavoriteJsManagerHelper()
+    //let emailSaved = UserDefaults().string(forKey: "savedEmail")
+    
+    
+    func readFavProducts() -> [PRODUCTJs] {
+        helper.readData { favItems in
+            self.favProducts = favItems
+        }
+        return favProducts
+    }
+    
+    func writeFavProducts(writeFavProduct: [PRODUCTJs]){
+        helper.writeData(favProduct: writeFavProduct)
+    }
+    
+}
